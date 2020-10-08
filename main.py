1364edacc3e67ddd6fcbeae83dfb2e88cdfa765f
avjoshi21 <avjoshi2@illinois.edu>
2020-10-08 02:03:53 -0500

    added 4th ordered spatial diff

--- main.py
+++ main.py
@@ -2,55 +2,42 @@ import numpy as np
 from matplotlib import pyplot as plt
 import copy
 import time
-# #finds and returns index and value of the array element closest to parameter
-# def findNearest(array, value,startIdx=0):
-#     array = np.asarray(array)
-#     bestValue = abs(array[startIdx] - value)
-#     # print(startIdx)
-#     bestIdx = startIdx
-#     prevIdx = startIdx-1
-#     nextIdx = startIdx+1
-#     # print(prevIdx)
-#     while(prevIdx >=0 or nextIdx<len(array)):
-#         if(prevIdx>=0):
-#             prevVal = array[prevIdx]
-#             if(abs(prevVal-value) < bestValue):
-#                 # print('found better one')
-#                 bestValue = abs(prevVal-value)
-#                 bestIdx = prevIdx
-#             prevIdx -= 1
-#         if(nextIdx<len(array)):
-#             nextVal = array[nextIdx]
-#             if(abs(nextVal-value) < bestValue):
-#                 bestValue = abs(nextVal-value)
-#                 bestIdx = nextIdx
-#             nextIdx += 1
 
-#     return (bestIdx,array[bestIdx])
 
 #second order derivative calculator. returns entire array with derivative at each point.
 def spatialDerivCalc(variableArray,delta,order=2):
     if order==2:
         paddedArray = np.pad(variableArray,1,mode='wrap')
+        paddedArray = np.pad(variableArray,1,mode='wrap')
+        fDiffArray = np.diff(paddedArray[1:])
+        bDiffArray = np.diff(paddedArray[::-1][1:])[::-1]
+        spatialDerivArray = (fDiffArray - bDiffArray)/(2*delta)
+
+        ## Slower code that does the same thing as above
+        # for count,i in enumerate(variableArray):
+        #     paddedCount = count+1
+        #     prevVal = paddedArray[paddedCount-1]
+        #     nextVal = paddedArray[paddedCount+1]
+        #     # currVal = i
+        #     spatialDerivArray[count] = (nextVal - prevVal)/(2*delta)
+
+
+    elif order==4:
+        paddedArray = np.pad(variableArray,2,mode='wrap')
         spatialDerivArray = np.zeros(variableArray.shape)
-        prevVal=0
-        # currVal=0
-        nextVal=0
         for count,i in enumerate(variableArray):
             paddedCount = count+1
-            prevVal = paddedArray[paddedCount-1]
-            nextVal = paddedArray[paddedCount+1]
+            # p1=previous1st n2=next2nd
+            p1Val = paddedArray[paddedCount-1]
+            n1Val = paddedArray[paddedCount+1]
+            p2Val = paddedArray[paddedCount-2]
+            n2Val = paddedArray[paddedCount+2]
             # currVal = i
-            spatialDerivArray[count] = (nextVal - prevVal)/(2*delta)
-    elif order==4:
-        1;
+            spatialDerivArray[count] = (-n2Val + 8*n1Val- 8*p1Val + p2Val)/(12* delta)
     
     return spatialDerivArray
 
 def piPsiPhiRK4(pi,psi,phi,deltaT,deltaX):
-    # finalPi = copy.deepcopy(pi)
-    # finalPsi = copy.deepcopy(psi)
-    # finalPhi = copy.deepcopy(phi)
 
     k1Pi = spatialDerivCalc(psi,deltaX)
     k1Psi = spatialDerivCalc(pi,deltaX)
@@ -75,96 +62,48 @@ def piPsiPhiRK4(pi,psi,phi,deltaT,deltaX):
     return finalPi,finalPsi,finalPhi
 
 
-# #performs a single rk4 step for an array of values. diffArrayFunc is a function passed that
-# #computes the derivative (discretized or continuous) by accepting the grid and the target value it should check
+if __name__ == "__main__":
+    #grid setup
+    hx = 0.01
+    ht = 0.0025
+    xLow = -2
+    xHigh = 2
+    tLow = 0
+    tHigh = 10
+    xGrid = np.arange(xLow,xHigh+hx,hx)
+    tGrid = np.arange(tLow,tHigh,ht)
 
-# def rk4(initialArray,gridArray,delta,diffArrayFunc,**diffArrayFuncArgs):
-#     # finalArray = np.zeros(len(initialArray))
-#     finalArray = copy.copy(initialArray)
-#     for count,val in enumerate(initialArray):
-#         y0 = val
-#         k1 = diffArrayFunc(count,**diffArrayFuncArgs)
-#         y1 = (y0 + delta/2 * k1)
-#         # y1Idx = findNearest(gridArray,y1)[0]
-#         y1Idx = findNearest(initialArray,y1,count)[0]
-#         k2 = diffArrayFunc(y1Idx,**diffArrayFuncArgs)
-#         y2 = (y0 + delta/2 * k2)
-#         # y2Idx = findNearest(gridArray,y2)[0]
-#         y2Idx = findNearest(initialArray,y2,y1Idx)[0]
-#         k3 = diffArrayFunc(y2Idx,**diffArrayFuncArgs)
-#         y3 = (y0 + delta * k3)
-#         # y3Idx = findNearest(gridArray,y3)[0]
-#         y3Idx = findNearest(initialArray,y3,y2Idx)[0]
-#         k4 = diffArrayFunc(y3Idx,**diffArrayFuncArgs)
-#         y4 = y0 + (k1 + 2*k2+ 2*k3 + k4)*delta/6
-#         finalArray[count] = y0 + delta*k1
-#         # finalArray[count] = y4
-#     return finalArray
-        
+    #initial values of the field phi and simulation variables psi and pi
+    initialPhi = np.exp(-((xGrid-0.5)/0.5)**2)
+    initialDtPhi = np.zeros(xGrid.shape)
+    initialDxPhi = spatialDerivCalc(initialPhi,hx)
+
+    initialPsi = initialDxPhi
+    initialPi = initialDtPhi
+
+    #set the arrays (t and x) of phi, psi and pi
+    phiArray = np.zeros((len(tGrid)+1,len(xGrid)))
+    psiArray = np.zeros((len(tGrid)+1,len(xGrid)))
+    piArray = np.zeros((len(tGrid)+1,len(xGrid)))
 
 
-#grid setup
-hx = 0.01
-ht = 0.0025
-xLow = -2
-xHigh = 2
-tLow = 0
-tHigh = 5
-xGrid = np.arange(xLow,xHigh+hx,hx)
-tGrid = np.arange(tLow,tHigh,ht)
+    phiArray[0] = initialPhi
+    psiArray[0] = initialPsi
+    piArray[0] = initialPi
 
-#initial values of the field phi and simulation variables psi and pi
-initialPhi = np.exp(-((xGrid-0.5)/0.5)**2)
-initialDtPhi = np.zeros(xGrid.shape)
-initialDxPhi = spatialDerivCalc(initialPhi,hx)
+    t1 = time.perf_counter()
+    for count,tVal in enumerate(tGrid):
+        piArray[count+1],psiArray[count+1],phiArray[count+1] = piPsiPhiRK4(piArray[count],psiArray[count],phiArray[count],ht,hx)
 
-initialPsi = initialDxPhi
-initialPi = initialDtPhi
-
-#set the arrays (t and x) of phi, psi and pi
-phiArray = np.zeros((len(tGrid)+1,len(xGrid)))
-psiArray = np.zeros((len(tGrid)+1,len(xGrid)))
-piArray = np.zeros((len(tGrid)+1,len(xGrid)))
-
-
-phiArray[0] = initialPhi
-psiArray[0] = initialPsi
-piArray[0] = initialPi
-
-# def derivCalc(idx,**kwargs):
-#     #if type is Psi, send the value of time derivative of psi, which is the spatial derivative of pi and
-#     #vice versa.
-#     if(kwargs['type']=='Pi'):
-#         return kwargs['dxPsi'][idx]
-#     elif(kwargs['type']=='Psi'):
-#         return kwargs['dxPi'][idx]
-#     elif(kwargs['type']=='Phi'):
-#         return kwargs['piArray'][idx]
-t1 = time.perf_counter()
-for count,tVal in enumerate(tGrid):
-    # dxPsi = spatialDerivCalc(psiArray[count],hx)
-    # dxPi = spatialDerivCalc(piArray[count],hx)
-    # if count == 1:
-    #     plt.plot(xGrid,dxPi)
-    
-    piArray[count+1],psiArray[count+1],phiArray[count+1] = piPsiPhiRK4(piArray[count],psiArray[count],phiArray[count],ht,hx)
-    # phiArray[count+1] = phiRK4(phiArray[count],piArray[count],ht,hx)
-    # piArray[count+1] = rk4(piArray[count],xGrid,ht,derivCalc,type='Pi',dxPsi=dxPsi)
-    # psiArray[count+1] = rk4 (psiArray[count],xGrid,ht,derivCalc,type='Psi',dxPi=dxPi)
-    # # print(count)
-    # phiArray[count+1] = rk4(phiArray[count],xGrid,ht,derivCalc,type='Phi',piArray=piArray[count])
-t2 = time.perf_counter()
-print('Integration finished in {:.4f} seconds. Now plotting...............'.format(t2-t1))
-for count,psiVals in enumerate(phiArray):
-    fig = plt.figure(count)
-    ax = plt.gca()
-    plt.xlabel('x')
-    plt.ylabel('Phi')
-    ax.set_ylim([-np.max(phiArray),np.max(phiArray)])
-    plt.plot(xGrid,psiVals,label="t={:.2f}".format(tGrid[count]))
-    plt.legend()
-    plt.savefig('Images/frame_{:04d}.png'.format(count))
-    plt.close(fig)
-
-
-
+    t2 = time.perf_counter()
+    print('Integration finished in {:.4f} seconds. Now plotting...............'.format(t2-t1))
+    for count,psiVals in enumerate(phiArray[::5]):
+        fig = plt.figure(count)
+        ax = plt.gca()
+        plt.xlabel('x')
+        plt.ylabel('Phi')
+        ax.set_ylim([-np.max(phiArray),np.max(phiArray)])
+        plt.plot(xGrid,psiVals,label="t={:.2f}".format(tGrid[count]))
+        plt.legend()
+        plt.savefig('Images/frame_{:04d}.png'.format(count))
+        plt.close(fig)
